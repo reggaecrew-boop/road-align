331f909a3395a7520b3ae35f669487f91c66624a
reggaecrew-boop <reggaecrew@gmail.com>
2026-02-03 10:35:43 +0900

    test

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'road-align-v18-6-1';
+const CACHE_NAME = 'road-align-v18-7';
 const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (event) => {
