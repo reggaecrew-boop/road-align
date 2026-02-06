395c24a5c27c2c8ec50574de4deba835c71d3f3d
reggaecrew-boop <reggaecrew@gmail.com>
2026-02-07 06:23:12 +0900

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-// sw.js (v18_9_7)
-const CACHE_NAME = "road-align-cache-v18_9_7";
+// sw.js (v18_9_8)
+const CACHE_NAME = "road-align-cache-v18_9_8";
 const CORE_ASSETS = [
   "./",
   "./index.html",
